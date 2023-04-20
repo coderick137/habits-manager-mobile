6b142781426e791d692eb686d452f9fefbcdd780
code_rick <ricardoanj.14@gmail.com>
2023-04-20 18:04:41 -0300

    para novo habito

---
+++ src/screens/New.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function New() {
+  return (
+    <View className='flex-1 bg-background px-8 pt-16'>
+      <Text className='text-2xl font-bold text-gray-800'>New Habit</Text>
+    </View>
+  )
+}
