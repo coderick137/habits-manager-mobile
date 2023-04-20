020c2eb221adb4e864c78f4394e5f51103c9081c
code_rick <ricardoanj.14@gmail.com>
2023-04-20 17:54:08 -0300

    utilizando dayjs em pt-br

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
+import './src/lib/dayjs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
