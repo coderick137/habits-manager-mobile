8999084eb1c0bf4d19b3a90a79efe79c885fc03f
code_rick <ricardoanj.14@gmail.com>
2023-04-20 16:54:47 -0300

    parte header da interface

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,27 +19,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <StatusBar
-      style="light"
-      backgroundColor="transparent"
-      translucent
-      />
-      <Text style={styles.text}>code_rick</Text>
-    </View>
+    <>
+      <Home />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: "#fff",
-    fontSize: 24,
-    fontFamily: "Inter_700Bold",
-  }
-});
